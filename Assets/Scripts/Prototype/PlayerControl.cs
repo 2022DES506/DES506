a3e86b569019700a8947ba86d5650df77afee396
多喝凉水。 <43728183+Folivora0707@users.noreply.github.com>
2022-10-03 15:28:24 +0100

    props

--- Assets/Scripts/Prototype/PlayerControl.cs
+++ Assets/Scripts/Prototype/PlayerControl.cs
@@ -9,7 +9,9 @@ public class PlayerControl : MonoBehaviour
     [SerializeField]
     private float jumpForce;
     [SerializeField]
-    private float bounceValue, greatJumpValue;  
+    private float bounceValue, greatJumpValue;
+    [SerializeField]
+    private float speedChangeTimer, speedChangeValue; 
 
     private bool isGround; 
 
@@ -21,6 +23,18 @@ public class PlayerControl : MonoBehaviour
 
     private int curDirection;
 
+    private struct speedChange
+    {
+        public float curTimer;
+        public float curValue; 
+    }
+    private List<speedChange> sc;
+
+    private bool isSlowDown;
+
+    private float totalChangeValue; 
+
+
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -28,11 +42,40 @@ public class PlayerControl : MonoBehaviour
         anim = GetComponent<Animator>();
 
         curDirection = 1;  // 1 == dash right, -1 == dash left 
+
+        sc = new List<speedChange>(); 
     }
 
     private void Update()
     {
-        GetInput(); 
+        GetInput();
+        SpeedChangeCheck(); 
+    }
+
+    private void SpeedChangeCheck()
+    {
+        if (sc==null || sc.Count == 0) return; 
+
+        for (int i=0; i<sc.Count; i++)
+        {
+            if (sc[i].curTimer <= 0)
+            {
+                sc.RemoveAt(i); 
+            }
+        }
+
+        totalChangeValue = 0f; 
+        for (int i=0; i<sc.Count; i++)
+        {
+            totalChangeValue += sc[i].curValue; 
+        }
+
+        for (int i=0; i<sc.Count; i++)
+        {
+            speedChange tempSC = sc[i];
+            tempSC.curTimer -= Time.deltaTime;
+            sc[i] = tempSC; 
+        }
     }
 
     private void GetInput()
@@ -53,7 +96,7 @@ public class PlayerControl : MonoBehaviour
     {
         if (isGround && !isJumping)
         {
-            rb.velocity = new Vector2(dashSpeed * curDirection, 0); 
+            rb.velocity = new Vector2((dashSpeed + totalChangeValue)* curDirection, 0); 
             anim.SetBool("isRun", true); 
         }
 
@@ -68,6 +111,8 @@ public class PlayerControl : MonoBehaviour
         }
     }
 
+
+    #region 碰撞和触发
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.collider.tag == "Ground")
@@ -98,21 +143,6 @@ public class PlayerControl : MonoBehaviour
 
     }
 
-    private void GoHigherLayer()
-    {
-        // 参数调整
-        GameManager.GM.curLevel++;
-        curDirection = -curDirection;
-        sr.flipX = !sr.flipX;
-
-        // 贴墙超级跳
-        isJumping = true; 
-        Vector2 newForce = Vector2.up;
-        newForce.x = curDirection * bounceValue;
-        newForce.y = Vector2.up.y * jumpForce * greatJumpValue;
-        rb.AddForce(newForce, ForceMode2D.Impulse);
-    }
-
     private void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.collider.tag == "Ground")
@@ -125,4 +155,61 @@ public class PlayerControl : MonoBehaviour
             rb.velocity = new Vector2(curDirection * bounceValue, jumpForce); 
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "SpeedUp")
+        {
+            speedChange newChange;
+            newChange.curTimer = speedChangeTimer;
+            newChange.curValue = speedChangeValue;
+            sc.Add(newChange);
+
+            Destroy(collision.gameObject); 
+        }
+
+        if (collision.tag == "SlowDown")
+        {
+            if (isSlowDown == false)
+            {
+                speedChange newChange;
+                newChange.curTimer = speedChangeTimer;
+                newChange.curValue = -speedChangeValue;
+                sc.Add(newChange); 
+
+                isSlowDown = true; 
+            }
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "SlowDown")
+        {
+            isSlowDown = false; 
+        }
+
+        if (collision.tag == "Hollow")
+        {
+            curDirection = -curDirection;
+            sr.flipX = !sr.flipX; 
+        }
+    }
+    #endregion
+
+    private void GoHigherLayer()
+    {
+        // 参数调整
+        GameManager.GM.curLevel++;
+        curDirection = -curDirection;
+        sr.flipX = !sr.flipX;
+
+        // 贴墙超级跳
+        isJumping = true;
+        Vector2 newForce = Vector2.up;
+        newForce.x = curDirection * bounceValue;
+        newForce.y = Vector2.up.y * jumpForce * greatJumpValue;
+        rb.AddForce(newForce, ForceMode2D.Impulse);
+    }
+
 }

356da8f355ebc966c27fe807cbf689b0d89a424f
多喝凉水。 <43728183+Folivora0707@users.noreply.github.com>
2022-10-06 15:02:31 +0100

    speed change logic & character animation

--- Assets/Scripts/ScriptsVin/PlayerControl.cs
+++ Assets/Scripts/ScriptsVin/PlayerControl.cs
@@ -4,36 +4,46 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    // 可调控变量
     [SerializeField]
-    private float dashSpeed;
+    private float dashSpeed, dashSpeedFast, dashSpeedSlow; 
     [SerializeField]
-    private float jumpForce;
+    private float jumpForce; 
     [SerializeField]
     private float bounceValue, greatJumpValue;
     [SerializeField]
-    private float speedChangeTimer, speedChangeValue; 
+    private float defaultTimer;
+    [SerializeField]
+    private float speedChangeTime, speedChangeRate;  
 
-    private bool isGround; 
+    // [SerializeField] 累加变速
+    // private float speedChangeTimer, speedChangeValue; 
 
+    // 状态判断
+    private bool isGround;
+    private bool isJumping;
+
+    // 组件
     private Rigidbody2D rb;
     private SpriteRenderer sr;
     private Animator anim;
 
-    private bool isJumping;
-
+    // 当前状态变量
     private int curDirection;
+    private float curSpeed;
+    private int curSpeedState; 
+    private float curTimer; 
 
+    /* 累加变速
     private struct speedChange
     {
         public float curTimer;
         public float curValue; 
     }
     private List<speedChange> sc;
-
     private bool isSlowDown;
-
     private float totalChangeValue; 
-
+    */
 
     private void Start()
     {
@@ -42,16 +52,64 @@ public class PlayerControl : MonoBehaviour
         anim = GetComponent<Animator>();
 
         curDirection = 1;  // 1 == dash right, -1 == dash left 
+        curSpeed = dashSpeed;
+        curSpeedState = 0; 
 
-        sc = new List<speedChange>(); 
+        // sc = new List<speedChange>(); // 累加变速
     }
 
     private void Update()
     {
         GetInput();
-        SpeedChangeCheck(); 
+        // SpeedChangeCheck(); // 累加变速
+        SpeedCheck(); 
     }
 
+    private void SpeedCheck()
+    {
+        curTimer -= Time.deltaTime; 
+        if (curTimer <= 0)
+        {
+            if (curSpeed != dashSpeed)
+            {
+                curSpeed = Mathf.Lerp(curSpeed, dashSpeed, speedChangeRate);
+                if (Mathf.Abs(curSpeed - dashSpeed) < 0.01f)
+                {
+                    curSpeed = dashSpeed; 
+                }
+            }
+        }
+        else
+        {
+            switch (curSpeedState)
+            {
+                case -1:
+                    curSpeed = Mathf.Lerp(dashSpeedSlow, curSpeed, 1 - speedChangeRate);
+                    if (Mathf.Abs(curSpeed - dashSpeedSlow) < 0.01f)
+                    {
+                        curSpeed = dashSpeedSlow;
+                    }
+                    break;
+                case 0:
+                    curSpeed = Mathf.Lerp(curSpeed, dashSpeed, speedChangeRate);
+                    if (Mathf.Abs(curSpeed - dashSpeed) < 0.01f)
+                    {
+                        curSpeed = dashSpeed;
+                    }
+                    break;
+                case 1:
+                    curSpeed = Mathf.Lerp(curSpeed, dashSpeedFast, speedChangeRate);
+                    if (Mathf.Abs(curSpeed - dashSpeedFast) < 0.01f)
+                    {
+                        curSpeed = dashSpeedFast; 
+                    }
+                    break;
+            }
+        }
+        
+    }
+
+    /* 累加变速
     private void SpeedChangeCheck()
     {
         if (sc==null || sc.Count == 0) return; 
@@ -77,6 +135,7 @@ public class PlayerControl : MonoBehaviour
             sc[i] = tempSC; 
         }
     }
+    */
 
     private void GetInput()
     {
@@ -96,8 +155,9 @@ public class PlayerControl : MonoBehaviour
     {
         if (isGround && !isJumping)
         {
-            rb.velocity = new Vector2((dashSpeed + totalChangeValue)* curDirection, 0); 
-            anim.SetBool("isRun", true); 
+            // rb.velocity = new Vector2((dashSpeed + totalChangeValue)* curDirection, 0); 
+
+            rb.velocity = new Vector2(curSpeed * curDirection, 0); 
         }
 
     }
@@ -152,12 +212,29 @@ public class PlayerControl : MonoBehaviour
 
         if (collision.collider.tag == "Obstacle")
         {
-            rb.velocity = new Vector2(curDirection * bounceValue, jumpForce); 
+            rb.velocity = new Vector2(curDirection * bounceValue, jumpForce  * greatJumpValue); 
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.tag == "SpeedUp")
+        {
+            curSpeed = Mathf.Lerp(curSpeed, dashSpeedFast, speedChangeRate); 
+            curTimer = defaultTimer;
+            curSpeedState = 1; 
+
+            Destroy(collision.gameObject); 
+        }
+
+        if (collision.tag == "SlowDown")
+        {
+            curSpeed = Mathf.Lerp(dashSpeedSlow, curSpeed, 1-speedChangeRate); 
+            curTimer = defaultTimer;
+            curSpeedState = -1; 
+        }
+
+        /* 累加变速
         if (collision.tag == "SpeedUp")
         {
             speedChange newChange;
@@ -180,14 +257,17 @@ public class PlayerControl : MonoBehaviour
                 isSlowDown = true; 
             }
         }
+        */
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
+        /* 累加变速
         if (collision.tag == "SlowDown")
         {
             isSlowDown = false; 
         }
+        */
 
         if (collision.tag == "Hollow")
         {
@@ -203,12 +283,13 @@ public class PlayerControl : MonoBehaviour
         GameManager.GM.curLevel++;
         curDirection = -curDirection;
         sr.flipX = !sr.flipX;
+        rb.velocity = Vector2.zero; 
 
         // 贴墙超级跳
         isJumping = true;
         Vector2 newForce = Vector2.up;
-        newForce.x = curDirection * bounceValue;
-        newForce.y = Vector2.up.y * jumpForce * greatJumpValue;
+        newForce.x = curDirection * bounceValue * 2;
+        newForce.y = Vector2.up.y * jumpForce * greatJumpValue * 2;
         rb.AddForce(newForce, ForceMode2D.Impulse);
     }
 

a1df7a0d48aa98c475d7c84426a1b1079ce53598
多喝凉水。 <43728183+Folivora0707@users.noreply.github.com>
2022-11-01 13:48:57 +0000

    moving door

---
+++ Assets/Scripts/ScriptsVin/MoveDoor.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveDoor : MonoBehaviour
+{
+    [SerializeField]
+    private float lapY1, lapY2, lapY3;
+
+    private void Update()
+    {
+        switch (GameManager.GM.curLap)
+        {
+            case 1:
+                transform.position = new Vector2(transform.position.x, lapY1); 
+                break;
+            case 2:
+                transform.position = new Vector2(transform.position.x, lapY2); 
+                break;
+            case 3:
+                transform.position = new Vector2(transform.position.x, lapY3); 
+                break;
+            default:
+                break;
+        }
+    }
+}

a1ecd6927d3ba2b28468af484fa09e8fb44feb03
多喝凉水。 <43728183+Folivora0707@users.noreply.github.com>
2022-11-08 15:32:01 +0000

    coins pick up animation

---
+++ Assets/Scripts/ScriptsVin/CoinFollowMe.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinFollowMe : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed = 0.1f;
+    [SerializeField]
+    private float height = 10f; 
+    [SerializeField]
+    private float timer = 0.5f; 
+    [SerializeField]
+    private float minDist = 0.1f;
+
+    private Vector2 endPos; 
+
+    private void Start()
+    {
+        endPos = new Vector2(transform.position.x, transform.position.y + height); 
+    }
+
+    private void Update()
+    {
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, endPos, speed);  
+        }
+        else
+        {
+            if (((Vector2)transform.position - GameManager.GM.playerPos).magnitude > minDist)
+            {
+                transform.position = Vector2.MoveTowards(transform.position, GameManager.GM.playerPos, speed);
+            }
+            else
+            {
+                Destroy(gameObject); 
+            }
+        }
+    }
+}

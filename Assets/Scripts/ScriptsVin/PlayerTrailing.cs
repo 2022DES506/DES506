237192f510ae326b73057af80c4d2c48937dac6a
多喝凉水。 <43728183+Folivora0707@users.noreply.github.com>
2022-11-11 15:28:56 +0000

    1

---
+++ Assets/Scripts/ScriptsVin/PlayerTrailing.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTrailing : MonoBehaviour
+{
+    private TrailRenderer tr;
+    [SerializeField]
+    private PlayerControl player; 
+
+    private void Start()
+    {
+        tr = GetComponent<TrailRenderer>(); 
+    }
+
+    private void Update()
+    {
+        if (player.isSpeedUping)
+        {
+            tr.enabled = false; 
+        }
+        else
+        {
+            tr.enabled = true; 
+            switch (GameManager.GM.curLap)
+            {
+                case 1:
+                    tr.startColor = Color.green;
+                    break;
+                case 2:
+                    tr.startColor = new Color(1f, 0.38f, 0f, 1f);
+                    break;
+                case 3:
+                    tr.startColor = Color.red;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
